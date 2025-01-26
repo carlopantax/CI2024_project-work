8826c75dbb8479c81d5443eabf48e90d43796e76
CARLO DI PANTALEO s333106 <s333106@studenti.polito.it>
2025-01-26 18:16:34 +0100

    Project working

---
+++ s333106.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+def f0(x: np.ndarray) -> np.ndarray:
+    return x[0] + np.sin(x[1]) / 5
+
+
+def f1(x: np.ndarray) -> np.ndarray:
+    return np.sin(x[0])
+
+def f2(x: np.ndarray) -> np.ndarray:
+    (np.exp(9.823) * np.exp((-5.612 / x[0])))
+
+
+def f3(x: np.ndarray) -> np.ndarray: ...
+
+
+def f4(x: np.ndarray) -> np.ndarray: ...
+
+
+def f5(x: np.ndarray) -> np.ndarray: ...
+
+
+def f6(x: np.ndarray) -> np.ndarray: ...
+
+
+def f7(x: np.ndarray) -> np.ndarray: ...
+
+
+def f8(x: np.ndarray) -> np.ndarray: ...
+
